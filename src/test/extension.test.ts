0531986fcf3f359ea6dd874c296ac2d6786e3e52
dark0dave <dark0dave@mykolab.com>
2025-05-10 11:53:36 +0100

    fix(dialogue): Fix dialogue parser
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,60 +1,17 @@
+import factory from "../models/factory";
 import * as assert from "assert";
-import are_parser from "../models/are";
-import bam_parser from "../models/bam";
-import cre_parser from "../models/cre";
-import dlg_parser from "../models/dlg";
-import eff_parser from "../models/eff";
-import itm_parser from "../models/itm";
-import spl_parser from "../models/spl";
-import sto_parser from "../models/sto";
-import vvc_parser from "../models/vvc";
 import * as fs from "fs";
 import * as path from "path";
-import * as vscode from "vscode";
 
 suite("Extension Test Suite", () => {
-  vscode.window.showInformationMessage("Start all tests.");
   const root: string = process.env.ROOT || ".";
 
-  const are_file = fs.readFileSync(
-    path.normalize(`${root}/fixtures/ar0002.are`),
-  );
   const are_file_json = JSON.parse(
     fs
       .readFileSync(path.normalize(`${root}/fixtures/ar0002.are.json`), "utf8")
       .trim(),
   );
 
-  const bam_v1_file = fs.readFileSync(
-    path.normalize(`${root}/fixtures/kit.bam`),
-  );
-  const bam_v1_json_file = JSON.parse(
-    fs
-      .readFileSync(path.normalize(`${root}/fixtures/kit.bam.json`), "utf8")
-      .trim(),
-  );
-
-  const bam_v2_file = fs.readFileSync(
-    path.normalize(`${root}/fixtures/ring.bam`),
-  );
-  const bam_v2_json_file = JSON.parse(
-    fs
-      .readFileSync(path.normalize(`${root}/fixtures/ring.bam.json`), "utf8")
-      .trim(),
-  );
-
-  const bamc_v1_file = fs.readFileSync(
-    path.normalize(`${root}/fixtures/bamc.bam`),
-  );
-  const bamc_v1_json_file = JSON.parse(
-    fs
-      .readFileSync(path.normalize(`${root}/fixtures/bamc.bam.json`), "utf8")
-      .trim(),
-  );
-
-  const cre_file = fs.readFileSync(
-    path.normalize(`${root}/fixtures/cutmelis.cre`),
-  );
   const cre_file_json = JSON.parse(
     fs
       .readFileSync(
@@ -64,18 +21,6 @@ suite("Extension Test Suite", () => {
       .trim(),
   );
 
-  const dlg_file = fs.readFileSync(
-    path.normalize(`${root}/fixtures/mazzy.dlg`),
-  );
-  const dlg_file_json = JSON.parse(
-    fs
-      .readFileSync(path.normalize(`${root}/fixtures/mazzy.dlg.json`), "utf8")
-      .trim(),
-  );
-
-  const eff_file = fs.readFileSync(
-    path.normalize(`${root}/fixtures/geniedam.eff`),
-  );
   const eff_file_json = JSON.parse(
     fs
       .readFileSync(
@@ -85,27 +30,18 @@ suite("Extension Test Suite", () => {
       .trim(),
   );
 
-  const itm_file = fs.readFileSync(
-    path.normalize(`${root}/fixtures/sw1h01.itm`),
-  );
   const itm_file_json = JSON.parse(
     fs
       .readFileSync(path.normalize(`${root}/fixtures/sw1h01.itm.json`), "utf8")
       .trim(),
   );
 
-  const spl_file = fs.readFileSync(
-    path.normalize(`${root}/fixtures/gate1.spl`),
-  );
   const spl_file_json = JSON.parse(
     fs
       .readFileSync(path.normalize(`${root}/fixtures/gate1.spl.json`), "utf8")
       .trim(),
   );
 
-  const sto_file = fs.readFileSync(
-    path.normalize(`${root}/fixtures/ohbconco.sto`),
-  );
   const sto_file_json = JSON.parse(
     fs
       .readFileSync(
@@ -115,9 +51,6 @@ suite("Extension Test Suite", () => {
       .trim(),
   );
 
-  const vvc_file = fs.readFileSync(
-    path.normalize(`${root}/fixtures/p_fireta.vvc`),
-  );
   const vvc_file_json = JSON.parse(
     fs
       .readFileSync(
@@ -129,71 +62,87 @@ suite("Extension Test Suite", () => {
 
   test("Area Tests", () => {
     assert.strictEqual(
-      JSON.stringify(are_parser.parse(are_file)),
+      JSON.stringify(factory(path.normalize(`${root}/fixtures/ar0002.are`))),
       JSON.stringify(are_file_json),
     );
   });
 
-  test("Bam Tests", () => {
-    assert.strictEqual(
-      JSON.stringify(bam_parser.parse(bam_v1_file)),
-      JSON.stringify(bam_v1_json_file),
-    );
-    assert.strictEqual(
-      JSON.stringify(bam_parser.parse(bam_v2_file)),
-      JSON.stringify(bam_v2_json_file),
-    );
-    assert.strictEqual(
-      JSON.stringify(bam_parser.parse(bamc_v1_file)),
-      JSON.stringify(bamc_v1_json_file),
-    );
+  ["bamc", "kit", "ring"].forEach((file) => {
+    test(`Bam Test: ${file}`, () => {
+      const expected = JSON.stringify(
+        JSON.parse(
+          fs
+            .readFileSync(
+              path.normalize(`${root}/fixtures/${file}.bam.json`),
+              "utf8",
+            )
+            .trim(),
+        ),
+      );
+      assert.strictEqual(
+        JSON.stringify(factory(path.normalize(`${root}/fixtures/${file}.bam`))),
+        expected,
+      );
+    });
   });
 
   test("Creature Tests", () => {
     assert.strictEqual(
-      JSON.stringify(cre_parser.parse(cre_file)),
+      JSON.stringify(factory(path.normalize(`${root}/fixtures/cutmelis.cre`))),
       JSON.stringify(cre_file_json),
     );
   });
 
-  test("Dialogue Tests", () => {
-    assert.strictEqual(
-      JSON.stringify(dlg_parser.parse(dlg_file)),
-      JSON.stringify(dlg_file_json),
-    );
+  ["mazzy", "zs_sdshd"].forEach((file) => {
+    test(`Dialogue Test: ${file}`, () => {
+      const expected = JSON.stringify(
+        JSON.parse(
+          fs
+            .readFileSync(
+              path.normalize(`${root}/fixtures/${file}.dlg.json`),
+              "utf8",
+            )
+            .trim(),
+        ),
+      );
+      assert.strictEqual(
+        JSON.stringify(factory(path.normalize(`${root}/fixtures/${file}.dlg`))),
+        expected,
+      );
+    });
   });
 
   test("Effect Tests", () => {
     assert.strictEqual(
-      JSON.stringify(eff_parser.parse(eff_file)),
+      JSON.stringify(factory(path.normalize(`${root}/fixtures/geniedam.eff`))),
       JSON.stringify(eff_file_json),
     );
   });
 
   test("Item Tests", () => {
     assert.strictEqual(
-      JSON.stringify(itm_parser.parse(itm_file)),
+      JSON.stringify(factory(path.normalize(`${root}/fixtures/sw1h01.itm`))),
       JSON.stringify(itm_file_json),
-    );
-  });
-
-  test("Store Tests", () => {
-    assert.strictEqual(
-      JSON.stringify(sto_parser.parse(sto_file)),
-      JSON.stringify(sto_file_json),
     );
   });
 
   test("Spell Tests", () => {
     assert.strictEqual(
-      JSON.stringify(spl_parser.parse(spl_file)),
+      JSON.stringify(factory(path.normalize(`${root}/fixtures/gate1.spl`))),
       JSON.stringify(spl_file_json),
+    );
+  });
+
+  test("Store Tests", () => {
+    assert.strictEqual(
+      JSON.stringify(factory(path.normalize(`${root}/fixtures/ohbconco.sto`))),
+      JSON.stringify(sto_file_json),
     );
   });
 
   test("VVC Tests", () => {
     assert.strictEqual(
-      JSON.stringify(vvc_parser.parse(vvc_file)),
+      JSON.stringify(factory(path.normalize(`${root}/fixtures/p_fireta.vvc`))),
       JSON.stringify(vvc_file_json),
     );
   });
